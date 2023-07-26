5da94d08a0794f80ea1d35579f46f2ae8b354b07
Abhiii2001 <abhimate2001@gmail.com>
2023-07-26 17:28:33 +0530

    First file Added

---
+++ Abhi.java
@@ -0,0 +1,40 @@
+class Abhi{
+
+
+
+
+int no = 56;
+String value = "abhishek";
+String add = "pimpalegaon";
+
+public void Display()
+{
+System.out.println("ABHUSHSSCvhsgfhifash");
+}
+
+public static void main(String[] args)
+{
+ Main n = new Main();
+
+n.Main();
+System.out.println(n.value);
+System.out.println(n.add);
+System.out.println(n.no);
+System.out.println("Abhi");
+
+n.Display();
+
+
+int no= 44521;
+String name= "ABHISHEK";
+
+System.out.println(no);
+System.out.println(name)
+
+
+
+
+
+
+}
+}
